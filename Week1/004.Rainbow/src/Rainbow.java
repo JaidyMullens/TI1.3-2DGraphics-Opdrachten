1fa2f0c052eeac2384b6835ece379c33805611ce
JaidyMullens <jaidymullens12@gmail.com>
2023-01-30 19:32:16 +0100

    Rainbow partially working
    Rainbow not fully 180 degrees, still need to figure the exact measurements out.

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -1,5 +1,8 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Group;
@@ -9,17 +12,45 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Rainbow extends Application {
+    Canvas canvas = new Canvas(1920, 1080);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Rainbow");
         primaryStage.show();
+
     }
     
     
-    public void draw(FXGraphics2D graphics) {
+    public void draw(FXGraphics2D graphics) throws InterruptedException {
+
+        double scale = 50.0;
+
+        // Set origin to middle under
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        graphics.scale(1, -1);
+        graphics.setColor(Color.blue);
+//        for(int i = 0; i < 500; i++) {
+//            graphics.setColor(Color.getHSBColor(i/500.0f, 1, 1));
+//            graphics.drawLine(i, 10, i, 100);
+//        }
+        float x1 = 0f;
+        float y1 = 0f;
+        float x2 = 0f;
+        float y2 = 0f;
+        int angle = 180;
+
+        for (float i = 0; i < 500f; i+= 0.1f) {
+            graphics.setColor(Color.getHSBColor(i/500.0f, 1, 1));
+            x1 = (float)(400 * Math.cos(i /angle));
+            y1 = (float)(400 * Math.sin(i/angle));
+            x2 = (float)(500 * Math.cos(i/angle));
+            y2 = (float)(500 * Math.sin(i/angle));
+            graphics.draw(new Line2D.Double(x1, y1, x2, y2));
+
+        }
 
     }
     
