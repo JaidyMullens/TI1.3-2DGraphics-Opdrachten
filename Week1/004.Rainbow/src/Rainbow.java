6edaf2459a64df0e135f2db8ff069845211ef352
JaidyMullens <jaidymullens12@gmail.com>
2023-01-31 08:55:49 +0100

    Rainbow done

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -40,24 +40,26 @@ public class Rainbow extends Application {
         float y1 = 0f;
         float x2 = 0f;
         float y2 = 0f;
-        float angle = 255;
+//        float angle = 159;
 
-        for (float i = 0; i < 500f; i+= 0.1f) {
-            graphics.setColor(Color.getHSBColor(i/500f, 1, 1));
-            x1 = (float)(400 * Math.cos(i / angle));
-            y1 = (float)(400 * Math.sin(i / angle));
-            x2 = (float)(500 * Math.cos(i / angle));
-            y2 = (float)(500 * Math.sin(i / angle));
-            graphics.draw(new Line2D.Double(x1, y1, x2, y2));
-        }
-//        for (float i = 0; i < Math.PI; i+= 0.1f) {
-//            graphics.setColor(Color.getHSBColor(i/500.0f, 1, 1));
-//            x1 = (float)(400 * Math.cos(i));
-//            y1 = (float)(400 * Math.sin(i));
-//            x2 = (float)(500 * Math.cos(i));
-//            y2 = (float)(500 * Math.sin(i));
+//        for (float i = 0; i < 500f; i+= 0.1f) {
+//            graphics.setColor(Color.getHSBColor(i/500f, 1, 1));
+//            x1 = (float)(400 * Math.cos(i / angle));
+//            y1 = (float)(400 * Math.sin(i / angle));
+//            x2 = (float)(500 * Math.cos(i / angle));
+//            y2 = (float)(500 * Math.sin(i / angle));
 //            graphics.draw(new Line2D.Double(x1, y1, x2, y2));
 //        }
+//        graphics.translate(this.canvas.getWidth() / 0.5, this.canvas.getHeight() / 0.5);
+
+        for (float i = 0; i < Math.PI; i+= 0.001f) {
+            graphics.setColor(Color.getHSBColor(i/(float)Math.PI, 1, 1));
+            x1 = (float)(400 * Math.cos(i));
+            y1 = (float)(400 * Math.sin(i));
+            x2 = (float)(500 * Math.cos(i));
+            y2 = (float)(500 * Math.sin(i));
+            graphics.draw(new Line2D.Double(x1, y1, x2, y2));
+        }
     }
     
     
