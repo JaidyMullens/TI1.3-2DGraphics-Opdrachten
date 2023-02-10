4aad745d910df05e97e0d9b4986b68c093c9746f
JaidyMullens <jaidymullens12@gmail.com>
2023-02-10 15:44:04 +0100

    Added a gradientpaint

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -5,8 +5,11 @@ import javafx.application.Application;
 
 import static javafx.application.Application.launch;
 
+import javafx.event.EventType;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
@@ -16,8 +19,7 @@ public class GradientPaintExercise extends Application {
     private ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -25,14 +27,29 @@ public class GradientPaintExercise extends Application {
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+        MouseEvent mouseEvent = new MouseEvent(null, null, MouseEvent.MOUSE_MOVED, );
+
     }
 
 
     public void draw(FXGraphics2D graphics)
     {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
-        graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+//        graphics.setTransform(new AffineTransform());
+
+        Point2D center = new Point2D.Double((double)canvas.getWidth() / 2, (double)canvas.getHeight() / 2);
+        float radius = (float)canvas.getWidth() / 2.0f;
+        float[] focus = {0.1f, 0.9f};
+        Color[] colors = {Color.red, Color.yellow};
+
+        RadialGradientPaint radialGradientPaint = new RadialGradientPaint(center, radius, focus, colors);
+
+//        RadialGradientPaint radialGradientPaint = new RadialGradientPaint(new Point2D.Double((double)canvas.getWidth() / 2, (double)canvas.getHeight() / 2),
+//                30, 40, new float[2], new Color[2], MultipleGradientPaint.CycleMethod.NO_CYCLE);
+
+        Rectangle2D rectangle = new Rectangle((int)canvas.getWidth(), (int)canvas.getHeight());
+        graphics.setPaint(radialGradientPaint);
+        graphics.fill(rectangle);
+        graphics.draw(rectangle);
     }
 
 
