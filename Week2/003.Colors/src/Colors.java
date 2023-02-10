ee7926b80ca13d6f5428189946dddbe10e50d0aa
JaidyMullens <jaidymullens12@gmail.com>
2023-02-10 12:56:57 +0100

    Algoritm made to draw 13 colored squares

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -14,7 +14,7 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Colors extends Application {
     private ResizableCanvas canvas;
-
+    private Color[] colors = new Color[13];
     @Override
     public void start(Stage primaryStage) throws Exception
     {
@@ -25,6 +25,21 @@ public class Colors extends Application {
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Colors");
         primaryStage.show();
+
+        colors[0] = Color.black;
+        colors[1] = Color.blue;
+        colors[2] = Color.cyan;
+        colors[3] = Color.darkGray;
+        colors[4] = Color.gray;
+        colors[5] = Color.green;
+        colors[6] = Color.lightGray;
+        colors[7] = Color.magenta;
+        colors[8] = Color.orange;
+        colors[9] = Color.pink;
+        colors[10] = Color.red;
+        colors[11] = Color.white;
+        colors[12] = Color.yellow;
+
     }
 
 
@@ -33,8 +48,17 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-    }
 
+        for (int i = 0; i < colors.length; i++) {
+            graphics.setColor(Color.black);
+            Area rectangle = new Area(new Rectangle2D.Double(50 + (i*70), 50, 50, 50));
+            graphics.draw(rectangle);
+
+            graphics.setColor(colors[i]);
+            graphics.fill(rectangle);
+        }
+
+    }
 
     public static void main(String[] args)
     {
