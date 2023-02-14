84645fd2bd41f6135f1237a4f0dab65acba27c8f
JaidyMullens <jaidymullens12@gmail.com>
2023-02-14 23:55:43 +0100

    Fading image made in loop
    Yet to fix the opacity of the the fading image not going to 0% for some reason

--- Week3/003.FadingImage/src/FadingImage.java
+++ Week3/003.FadingImage/src/FadingImage.java
@@ -8,6 +8,7 @@ import static javafx.application.Application.launch;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.transform.Affine;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
 import org.jfree.fx.FXGraphics2D;
@@ -15,7 +16,13 @@ import org.jfree.fx.ResizableCanvas;
 
 public class FadingImage extends Application {
     private ResizableCanvas canvas;
-    
+    private BufferedImage imgTheRock;
+    private BufferedImage imgTheCow;
+    private static float fadeSpeed = 0.007f;
+    private float transparency = 0.0f;
+    private float bufferTime = 0f;
+    boolean bufferIsOn = false;
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -23,6 +30,9 @@ public class FadingImage extends Application {
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+
+        init();
+
         new AnimationTimer() {
             long last = -1;
             @Override
@@ -43,14 +53,65 @@ public class FadingImage extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
-        graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
-        graphics.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
+//        graphics.setTransform(new AffineTransform());
+//        graphics.setBackground(Color.white);
+//        graphics.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
+
+        AffineTransform transpartentImg = new AffineTransform();
+        AffineTransform backgroundImg = new AffineTransform();
+//        transpartentImg.translate(transpartentImg.getTranslateX() + 500, transpartentImg.getTranslateY());
+
+        graphics.drawImage(imgTheCow, backgroundImg,null);
+
+
+
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, transparency));
+
+        graphics.drawImage(imgTheRock, transpartentImg,null);
+
+
     }
     
 
     public void update(double deltaTime) {
-	
+
+        if (!bufferIsOn) {
+            transparency += fadeSpeed;
+
+            if (transparency > 0.95f) {
+                bufferIsOn = true;
+                fadeSpeed *= -1;
+            }
+
+            if (transparency <= 0f)
+            {
+                System.out.println(transparency);
+                System.out.println("The rock is only visible");
+                fadeSpeed *= -1;
+                bufferIsOn = true;
+            }
+        }
+        else{
+            // Buffer is on
+         bufferTime += 0.1f;
+         if (bufferTime >= 8f)
+         {
+             bufferIsOn = false;
+             bufferTime = 0;
+         }
+        }
+
+    }
+
+    public void init() throws IOException {
+        transparency = 0.15f;
+
+        imgTheRock = ImageIO.read(getClass().getResource("images/rock.jpg"));
+        imgTheRock.getScaledInstance(300, 900, Image.SCALE_SMOOTH);
+
+        imgTheCow = ImageIO.read(getClass().getResource("images/cow.jpg"));
+//        imgTheCow.getScaledInstance(900, 900, Image.SCALE_SMOOTH);
+
     }
 
     public static void main(String[] args) {
