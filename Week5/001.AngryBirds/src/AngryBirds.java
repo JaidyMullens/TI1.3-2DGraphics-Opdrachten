b746b5a06b6e4001fe2e735eaea88d86ef60dfd3
JaidyMullens <jaidymullens12@gmail.com>
2023-03-12 22:27:43 +0100

    Added sprites, but background color is acting up

--- Week5/001.AngryBirds/src/AngryBirds.java
+++ Week5/001.AngryBirds/src/AngryBirds.java
@@ -1,6 +1,10 @@
 
 import java.awt.*;
 import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -22,6 +26,8 @@ import org.dyn4j.geometry.Vector2;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
+import javax.imageio.ImageIO;
+
 public class AngryBirds extends Application {
 
     private ResizableCanvas canvas;
@@ -31,11 +37,12 @@ public class AngryBirds extends Application {
     private boolean debugSelected = false;
     private ArrayList<GameObject> gameObjects = new ArrayList<>();
     private Vector2 ropeLength;
-    private PulleyJoint catapultJoint;
+    private PinJoint catapultJoint;
     private Body catapult;
     private Body ball;
     private double catapultForce;
     private boolean cataPultReleased = false;
+    private BufferedImage background;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -55,6 +62,18 @@ public class AngryBirds extends Application {
 
         camera = new Camera(canvas, g -> draw(g), g2d);
         mousePicker = new MousePicker(canvas);
+
+        try {
+            background = ImageIO.read(getClass().getResourceAsStream("/img/sky_2.jpg"));
+
+            for (String str : ImageIO.getReaderFormatNames()) {
+                System.out.println(str);
+            }
+
+//            System.out.println(getClass().getResourceAsStream("/img/mountains.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         canvas.setOnMouseReleased(event -> {
             if (mousePicker.getBody() != null)
@@ -93,36 +112,51 @@ public class AngryBirds extends Application {
 
         Body floor = new Body();
         floor.addFixture(Geometry.createRectangle(100, 3));
-        floor.getTransform().setTranslation(0, -5);
+        floor.getTransform().setTranslation(0, 0);
         floor.setMass(MassType.INFINITE);
         world.addBody(floor);
+        gameObjects.add(new GameObject("/img/floor.jpg", floor, new Vector2(0, 247), 1.255));
+
+
+        Body background = new Body();
+        background.addFixture(Geometry.createRectangle(100, 100));
+        background.getTransform().setTranslation(0, 100);
+        background.setMass(MassType.INFINITE);
 
         ball = new Body();
         ball.addFixture(Geometry.createCircle(0.3));
         ball.getTransform().setTranslation(new Vector2(-4,4));
         ball.setMass(MassType.NORMAL);
         ball.getFixture(0).setRestitution(.250);
-//        ball.applyImpulse(-20);
         world.addBody(ball);
+        gameObjects.add(new GameObject("/img/steve_head.png", ball, new Vector2(0,0), 0.052));
 
         catapult = new Body();
-//        catapult.addFixture(Geometry.createCircle(0.1));
         catapult.getTransform().setTranslation(new Vector2(-4,4));
         catapult.setMass(MassType.INFINITE);
         world.addBody(catapult);
 
-//
-//        catapultJoint = new PinJoint(ball, catapult.getTransform().getTranslation(), 10, 0, 200);
-//        catapultJoint.setTarget(this.catapult.getTransform().getTranslation());
-//        catapultJoint.
-//        catapultJoint.setCollisionAllowed(false);
-//        world.addJoint(catapultJoint);
 
-        catapultJoint = new PulleyJoint(catapult, ball, new Vector2(-4,4), ball.getWorldCenter(), new Vector2(-4,4), ball.getWorldCenter());
+        catapultJoint = new PinJoint(ball, catapult.getTransform().getTranslation(), 10, 0, 200);
+        catapultJoint.setTarget(this.catapult.getTransform().getTranslation());
         catapultJoint.setCollisionAllowed(false);
-        catapultJoint.setSlackEnabled(true);
-        catapultJoint.setLength(0.1);
         world.addJoint(catapultJoint);
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10-y; x++) {
+                Body box = new Body();
+                box.addFixture(Geometry.createRectangle(.25, .25));
+                box.setMass(MassType.INFINITE);
+                box.getTransform().setTranslation(5 + x * 0.25 + 0.125 * y, y*0.25);
+                world.addBody(box);
+            }
+        }
+
+//        catapultJoint = new PulleyJoint(catapult, ball, new Vector2(-4,4), ball.getWorldCenter(), new Vector2(-4,4), ball.getWorldCenter());
+//        catapultJoint.setCollisionAllowed(false);
+//        catapultJoint.setSlackEnabled(true);
+//        catapultJoint.setLength(0.1);
+//        world.addJoint(catapultJoint);
 
 
     }
@@ -134,6 +168,15 @@ public class AngryBirds extends Application {
 
         graphics.setTransform(camera.getTransform((int)canvas.getWidth(), (int)canvas.getHeight()));
         graphics.scale(1,-1);
+        AffineTransform imageTransform = new AffineTransform();
+//        imageTransform.translate(1, -1);
+//        imageTransform.transform(new Point2D.Double(1000,1000), new Point2D.Double(1000, 100));
+
+        imageTransform.scale(1, -1);
+        imageTransform.scale(4, 4);
+        imageTransform.translate(-700, -600);
+        graphics.drawImage(background,  imageTransform, null);
+        graphics.setBackground(Color.getHSBColor(213, 44, 100));
 
         if (!cataPultReleased)
         {
@@ -147,7 +190,7 @@ public class AngryBirds extends Application {
 
         }
 
-        DebugDraw.draw(graphics, world, 100);
+//        DebugDraw.draw(graphics, world, 100);
         for (GameObject go : gameObjects) {
             go.draw(graphics);
         }
@@ -164,7 +207,7 @@ public class AngryBirds extends Application {
         totalFrameTime += deltaTime;
 //        catapultJoint.setLength(ballToJoint.difference());
         double ropeLength = calculateVectorLength(ball.getTransform().getTranslation(), new Vector2(catapultJoint.getAnchor1().x, catapultJoint.getAnchor1().y));
-        catapultJoint.setLength(ropeLength);
+//        catapultJoint.setLength(ropeLength);
 //
 //        if (ropeLength > 0.001)
 //            catapultJoint.setLength(0.001);
@@ -172,13 +215,13 @@ public class AngryBirds extends Application {
         if (cataPultReleased) {
 
             Vector2 ballToJoint = catapultJoint.getAnchor1().subtract(mousePicker.getBody().getTransform().getTranslation());
-
 //            Vector2 ballToJoin = new Vectore
             double distance = ballToJoint.getMagnitude();
             double magnitude = Math.max(100, Math.min(distance * 10, 10000));
-            System.out.println(ball.applyForce(ballToJoint.multiply(magnitude)));
+//            System.out.println(ball.applyForce(ballToJoint.multiply(magnitude)));
             world.removeJoint(catapultJoint);
             ball.applyImpulse(-0.002);
+            ball.applyForce(ballToJoint.multiply(magnitude));
             mousePicker.mousePos = null;
             cataPultReleased = false;
 //            cataPultReleased = false;
